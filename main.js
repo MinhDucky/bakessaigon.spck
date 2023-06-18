2972e3cb1be1b9bd2a91c553f9e2865265d08221
MinhDucky <mn345289@gmail.com>
2023-06-18 16:48:05 +0700

    update website
    A wonderful day i make this code

---
+++ main.js
@@ -0,0 +1,20 @@
+let nav = document.getElementById("nav")
+
+window.addEventListener('scroll', function() {
+    console.log(window.scrollY)
+    if(window.scrollY > 10){
+        
+        console.log("scroll")
+        nav.style.top = "0"
+        nav.style.marginTop = '-10px'
+        nav.style.backgroundColor = "rgb(255 255 255 / 0.3)"
+        nav.style.backdropFilter = "blur(5px)"
+
+    }
+    else{
+        nav.style.backgroundColor = "white"
+        nav.style.marginTop = "20px"
+
+    }
+}
+)
